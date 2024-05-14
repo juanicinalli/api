13b24476ee609e429bdfbd7663c33de2ce0d11ae
juani <juani.cinalli@gmail.com>
2024-05-14 05:26:15 -0300

    deleted app service and controller

--- apps/login/src/app.module.ts
+++ apps/login/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -12,7 +10,7 @@ import configuration from 'config/configuration';
     UserModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
